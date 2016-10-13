dc2d3be3340fb37c758db921c5996452e8e9f77e
ktevin01 <tevinpowell@gmail.com>
2016-10-13 14:52:39 -0500

    Part 1

--- maze.js
+++ maze.js
@@ -1,1 +1,17 @@
-var test = 1;
+window.onload = function main (){
+	//alert('Hey How you doing');
+	var edge = document.getElementById('boundary1');
+	function mouseon(){
+		edge.setAttribute("style","border-color: red");
+	};
+	function mouseoff(){
+		edge.setAttribute("style","border-color: black");
+		};
+	edge.onmouseover=function(e) {
+	        mouseon();
+	};
+	edge.onmouseout=function(e) {
+	        mouseoff();
+	};
+
+};
