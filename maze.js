3ec149ad04901d7fa371098ffc4587f259a42a95
ktevin01 <tevinpowell@gmail.com>
2016-10-14 16:12:38 -0500

    Part 6

--- maze.js
+++ maze.js
@@ -3,7 +3,14 @@ window.onload = function main (){
 	var lose=false;
 	var end = document.getElementById('end');
 	var start=document.getElementById('start');
-	var init=document.getElementById("status").textContent;
+	var init="Maze Time";
+	var maze=document.getElementById("maze");
+
+	for (var h=0;h< edges.length;h++){
+		//console.log(h);
+		edges[h].setAttribute("class","youlose boundary");
+		document.getElementById("status").innerHTML = "Ready, Lets go";
+	}
 
 	for(var i=0;i< edges.length;i++){
 		edges[i].onmouseover=function(){
@@ -13,7 +20,7 @@ window.onload = function main (){
 				for (var h=0;h< edges.length;h++){
 					//console.log(h);
 					edges[h].setAttribute("class","youlose boundary");
-					document.getElementById("status").innerHTML = "Try again buddy, you lose";
+					document.getElementById("status").innerHTML = "Try again buddy, you lose. Press 'S' to try again";
 				}
 			}
 		}
@@ -23,7 +30,7 @@ window.onload = function main (){
 				document.getElementById("status").innerHTML = "Winner Winner chicken dinner!";
 				//alert ('Way to go. \n Now please wait one decade for your fake glod medal.');
 			}else{
-				document.getElementById("status").innerHTML = "Try again buddy, you lose";
+				document.getElementById("status").innerHTML = "Try again buddy, you lose. Press 'S' to try again";
 				//alert ('That was a failure and you might not survive the winter');
 			}
 	};
@@ -32,8 +39,29 @@ window.onload = function main (){
 		//alert("3,2,1 GOOO!!!");
 		for (var h=0;h< edges.length;h++){
 			edges[h].setAttribute("class","boundary");
-			lose=false;
-			document.getElementById("status").innerHTML =init;
 		}
+		lose=false;
+		document.getElementById("status").innerHTML =init;
 	};
+	maze.onmousemove=function(num){
+			var right=maze.offsetLeft+maze.offsetWidth;
+			var left=maze.offsetLeft;
+			var x = num.clientX;
+			var y = num.clientY;
+			console.log(right,x);
+			//if(x<left){
+			if(x<=left||x>=right-2){
+				//alert('yes');
+				for (var h=0;h< edges.length;h++){
+					edges[h].setAttribute("class","youlose boundary");
+					// document.getElementById("status").innerHTML = "Try again buddy, you lose";
+					if(lose){
+						document.getElementById("status").innerHTML = "And back to start you go";
+					}
+				}
+				lose=true;
+			}
+
+	};
+
 };
