c4841ea97e2691081901fd3ae7d7811ce8af13ca
ktevin01 <tevinpowell@gmail.com>
2016-10-14 11:37:13 -0500

    Part 4

--- maze.js
+++ maze.js
@@ -2,6 +2,7 @@ window.onload = function main (){
 	var edges = document.querySelectorAll(".boundary");
 	var lose=false;
 	var end = document.getElementById('end');
+	var start=document.getElementById('start');
 
 	for(var i=0;i< edges.length;i++){
 		edges[i].onmouseover=function(){
@@ -17,9 +18,17 @@ window.onload = function main (){
 	}
 	end.onmouseover=function(){
 			if (lose===false){
-				alert ('yay');
+				alert ('Way to go. \n Now please wait one decade for your fake glod medal.');
 			}else{
-				alert ('boo');
+				alert ('That was a failure and you might not survive the winter');
 			}
+	};
+
+	start.onclick=function(){
+		//alert("3,2,1 GOOO!!!");
+		for (var h=0;h< edges.length;h++){
+			edges[h].setAttribute("class","boundary");
+			lose=false;
+		}
 	};
 };
